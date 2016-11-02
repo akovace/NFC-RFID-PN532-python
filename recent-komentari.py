61fb1c873ab15a3646cf9185530b7711f84af198
akovace <akovace@unipu.hr>
2016-11-03 00:17:48 +0100

    komentari

--- recent-komentari.py
+++ recent-komentari.py
@@ -5,26 +5,27 @@ import os
 import requests
 
 def nfcuid():
-	threading.Timer(1.0, nfcuid).start()
-	string1 = eb_to_list() 
-	if(string1[2][0] == "R"): 
-		jsonfile = list_to_json(string1)
-		if(len(jsonfile) == 19):
-			os.system('aplay futurebeep3.wav')	
+	threading.Timer(1.0, nfcuid).start() # Timer koji okida funkciju svake 1 sekunde
+	string1 = eb_to_list() # Funkcija za dohvacanje podataka iz os-a i njihovo pretvaranje u listu
+
+	if(string1[2][0] == "R"): # Ako lista sadrzi trazeni podatak
+		jsonfile = list_to_json(string1) # U listi nadi trzeni podatak i pretvori u json
+		if(len(jsonfile) == 19): # Trazeni podatak treba imati 19 znakova
+			os.system('aplay futurebeep3.wav') # zvuk za dohvacanje podataka s xice	
 			try:
-				response = sent_json_rest(jsonfile) 
-				if(response[0] == 200):
-					os.system('aplay accessgranted2.wav')
+				response = sent_json_rest(jsonfile) # podatak saljemo na rest 
+				if(response[0] == 200): # potvrdan odgovor
+					os.system('aplay accessgranted2.wav') #potvrdan zvuk
 					print response
 				else: 
-					os.system('aplay accessdeniedfemale.wav')
+					os.system('aplay accessdeniedfemale.wav') #Osobe nema u sustavu
 			except:
 				print "nema interneta"
 				os.system('aplay modem.wav')
 			
 				
 		else:
-			os.system('aplay bleep_04.wav')
+			os.system('aplay bleep_04.wav') #nisu stigli svi podaci s xice
 	      	
 
 	return
@@ -51,4 +52,4 @@ def sent_json_rest(json):
 	return(r.status_code, r.json())
 
 
-nfcuid()
+nfcuid() # Poziv glavne funkcije
